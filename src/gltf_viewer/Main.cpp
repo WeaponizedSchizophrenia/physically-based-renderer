df135e4953987ab2eec7e0f31b9aa6376b37de76
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2024-12-26 14:06:12 +0200

    Add command line argument

--- src/gltf_viewer/Main.cpp
+++ src/gltf_viewer/Main.cpp
@@ -1,6 +1,18 @@
+#include <filesystem>
+#include <iterator>
 #include <print>
+#include <span>
 
-auto main() -> int {
-  std::println("Hello, world!");
+namespace {
+using ArgvType = char const* const*;
+}
+
+auto main(int const argc, ArgvType const argv) -> int {
+  std::span const args(std::next(argv), argc - 1);
+
+  if (!args.empty()) {
+    std::filesystem::path const gltfFilePath(args.front());
+    std::println("GLTF file: {}", gltfFilePath.c_str());
+  }
   return 0;
 }
