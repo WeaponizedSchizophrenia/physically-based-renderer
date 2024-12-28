e86eb9b9695916ceb7e5c562984acdac8865b325
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2024-12-28 13:15:02 +0200

    Remove render coroutine

--- tests/PbrEngine_Tests.cpp
+++ tests/PbrEngine_Tests.cpp
@@ -1,16 +1,16 @@
 #include <catch2/catch_test_macros.hpp>
-#include <utility>
-#include <vulkan/vulkan_enums.hpp>
+
+#include "pbr/utils/Conversions.hpp"
+
+#include "pbr/core/GpuHandle.hpp"
 
 #include "pbr/AsyncSubmitInfo.hpp"
 #include "pbr/AsyncSubmitter.hpp"
-#include "pbr/RenderCoroutine.hpp"
-#include "pbr/core/GpuHandle.hpp"
-#include "pbr/utils/Conversions.hpp"
-
 #include "pbr/Surface.hpp"
 
 #include "vkfw/vkfw.hpp"
+
+#include <utility>
 
 TEST_CASE("Surface creation", "[pbr]") {
   [[maybe_unused]]
@@ -26,32 +26,6 @@ TEST_CASE("Surface creation", "[pbr]") {
   pbr::Surface const surface(
       gpu, vkfw::createWindowSurfaceUnique(gpu->getInstance(), window.get()),
       pbr::utils::toExtent(window->getFramebufferSize()));
-}
-
-TEST_CASE("Render coroutine", "[pbr]") {
-  [[maybe_unused]]
-  auto const vkfw = vkfw::initUnique({.platform = vkfw::Platform::eX11});
-
-  auto const gpu = pbr::core::makeGpuHandle({
-      .extensions = vkfw::getRequiredInstanceExtensions(),
-      .presentPredicate = vkfw::getPhysicalDevicePresentationSupport,
-      .enableValidation = true,
-  });
-
-  auto const commandPool = gpu->getDevice().createCommandPoolUnique({
-      .flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
-      .queueFamilyIndex = gpu->getPhysicalDeviceProperties().graphicsTransferPresentQueue,
-  });
-  auto cmdBuffers = gpu->getDevice().allocateCommandBuffersUnique({
-      .commandPool = commandPool.get(),
-      .commandBufferCount = 1,
-  });
-  auto coroutine = pbr::createRenderCoroutine(gpu, std::move(cmdBuffers.front()));
-
-  coroutine.render();
-  coroutine.render();
-
-  coroutine.flush();
 }
 
 TEST_CASE("Async submit", "[pbr]") {
