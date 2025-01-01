0eea1c3ac6ffed3b21710e72d54af38971c5c511
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2025-01-01 14:44:15 +0200

    Fix type in input locations

--- assets/shaders/imgui/vertex.glsl
+++ assets/shaders/imgui/vertex.glsl
@@ -2,7 +2,7 @@
 
 layout(location = 0) in vec2 inPosition;
 layout(location = 1) in vec2 inUV;
-layout(location = 3) in vec4 inColor;
+layout(location = 2) in vec4 inColor;
 
 layout(location = 0) out vec2 outUV;
 layout(location = 1) out vec4 outColor;
